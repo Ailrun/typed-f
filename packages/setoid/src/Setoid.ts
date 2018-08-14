5abdbe5f4000d1e0ff71b15881dcf293c0482126
Junyoung Clare Jang <jjc9310@gmail.com>
2018-08-13 21:59:43 -0400

    feat(package): update packages to use @typed-f/tagged package

--- packages/setoid/src/Setoid.ts
+++ packages/setoid/src/Setoid.ts
@@ -1,10 +1,12 @@
-export interface Setoid<_T> {
-  /**
-   * @desc
-   * `Setoid` methods
-   */
+import * as T from '@typed-f/tagged';
 
-  equals<U>(other: Setoid<U>): boolean;
+interface SetoidBuilder<Other> {
+  equals(other: Other): boolean;
 
-  notEquals<U>(other: Setoid<U>): boolean;
+  notEquals(other: Other): boolean;
 }
+
+export interface Setoid1<Tag extends keyof T.Tag1List<any>> extends T.Tagged<Tag>, SetoidBuilder<T.Tag1List<any>[Tag]> {}
+export interface Setoid2<Tag extends keyof T.Tag2List<any, any>> extends T.Tagged<Tag>, SetoidBuilder<T.Tag2List<any, any>[Tag]> {}
+export interface Setoid3<Tag extends keyof T.Tag3List<any, any, any>> extends T.Tagged<Tag>, SetoidBuilder<T.Tag3List<any, any, any>[Tag]> {}
+export interface Setoid4<Tag extends keyof T.Tag4List<any, any, any, any>> extends T.Tagged<Tag>, SetoidBuilder<T.Tag4List<any, any, any, any>[Tag]> {}
