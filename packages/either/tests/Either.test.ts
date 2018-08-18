88c43949755de1a6c0508219a3c654dcea2de8df
Junyoung Clare Jang <jjc9310@gmail.com>
2018-08-18 16:16:26 -0400

    test(either): add linting script for tests

--- packages/either/tests/Either.test.ts
+++ packages/either/tests/Either.test.ts
@@ -17,32 +17,32 @@ describe('Either', () => {
       const sequenceObject = Either.sequenceObject;
 
       it('should return a Right of object when input is an object of Rights', () => {
-        expect(sequenceObject({
+        expect(sequenceObject<any, { x: number; y: number; z: number }>({
           x: new Right(5),
           y: new Right(4),
           z: new Right(3),
-        })).toEqual(new Right({
+        })).toEqual(new Right<any, { x: number; y: number; z: number }>({
           x: 5,
           y: 4,
           z: 3,
         }));
 
-        expect(sequenceObject({
+        expect(sequenceObject<any, { name: string; footSize: number; address: string }>({
           name: new Right('Junyoung Clare Jang'),
           footSize: new Right(4000),
           address: new Right('Somewhere near heaven'),
-        })).toEqual(new Right({
+        })).toEqual(new Right<any, { name: string; footSize: number; address: string }>({
           name: 'Junyoung Clare Jang',
           footSize: 4000,
           address: 'Somewhere near heaven',
         }));
       });
 
-      it('should return a Left of first error when input includes more than one Lefts', () => {
-        expect(sequenceObject<string, { world: string, mine: any }>({
-          world: new Right('is'),
-          mine: new Left('sing'),
-        })).toEqual(new Left('sing'));
+      it('should return a Left of first Left when input includes more than one Lefts', () => {
+        expect(sequenceObject({
+          world: new Right<string, string>('is'),
+          mine: new Left<string, any>('sing'),
+        })).toEqual(new Left<string, any>('sing'));
       });
     });
 
@@ -54,7 +54,7 @@ describe('Either', () => {
           new Right(1),
           new Right(2),
           new Right('a'),
-        ])).toEqual(new Right([
+        ])).toEqual(new Right<any, (number | string)[]>([
           1,
           2,
           'a',
@@ -63,7 +63,7 @@ describe('Either', () => {
         expect(sequenceArray<any, { x: number } | RegExp>([
           new Right({ x: 4 }),
           new Right(/ab*/),
-        ])).toEqual(new Right([
+        ])).toEqual(new Right<any, ({ x: number } | RegExp)[]>([
           { x: 4 },
           /ab*/,
         ]));
